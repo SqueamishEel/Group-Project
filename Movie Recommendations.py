358170226c280058104809660556f31ef2558e33
SqueamishEel <dzaccy@outlook.com>
2026-02-03 10:46:31 +0000

    Update Movie Recommendations.py

--- Movie Recommendations.py
+++ Movie Recommendations.py
@@ -1,7 +1,8 @@
 #Import Components
 import pandas as pd
+import numpy as np
 
-#Read dataset files
+#Read dataset Fils
 FullDataset = pd.read_csv("C:/Users/25719211/OneDrive - Edge Hill University/Pro - Practice II/movies_dataset.csv")
 MovieDataset = pd.read_csv("C:/Users/25719211/OneDrive - Edge Hill University/Pro - Practice II/movies_dataset.csv")
 
@@ -15,6 +16,9 @@ MovieDataset.pop("NumVotesIMDb")
 MovieDataset.pop("ReleaseYear")
 MovieDataset.pop("NumVotesRT")
 
-#Output datasets
+#Replacing Missing Values with NaN
+MovieDataset['IMDbRating'] = MovieDataset['IMDbRating'].replace(0, np.nan)
+MovieDataset['IMDbRating'].fillna(MovieDataset['IMDbRating'].median(), inplace = True)
+
+#Output Datasets
 FullDataset
-MovieDataset
