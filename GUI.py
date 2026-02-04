47d2c98444e19b49f34182ae305ff4eee40d4c68
SqueamishEel <dzaccy@outlook.com>
2026-02-04 11:16:58 +0000

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,14 +1,50 @@
 import tkinter as tk
+from tkinter import Tk, Label
 
 window = tk.Tk()
 
 window.title("Movie recommendation system")
 window.geometry("500x500")
-window.configure(background="black")
-tk.Label(text="genre").pack()
-tk.Label(text="actors").pack()
-tk.Label(text="director").pack()
-tk.Label(text="Rating").pack()
-tk.Label(text="Release Date").pack()
-window.mainloop()
+window.configure(background="lightblue")
 
+#Page title
+title_var = tk.StringVar()
+title_var.set("Find A Recommended Movie​")
+titlelabel = tk.Label(window, 
+                 textvariable=title_var, 
+                 anchor=tk.CENTER,       
+                 bg="darkblue",      
+                 height=2,              
+                 width=30,              
+                 bd=3,                  
+                 font=("Arial", 16, "bold"), 
+                 cursor="hand2",   
+                 fg="white",                          
+                 justify=tk.CENTER,    
+                 relief=tk.RAISED,     
+                 wraplength=2500        
+                )
+titlelabel.pack(pady=20)
+
+label_genre = Label(window, text="Genre",
+                    anchor="w",
+                    font=("Arial", 16, "bold"),       
+                    bg="lightblue",   
+                    fg="black")
+label_genre.pack(pady=10, padx=10, anchor="w")
+
+label_Actor = Label(window, text="Actor",
+                    anchor="w",
+                    font=("Arial", 16, "bold"),       
+                    bg="lightblue",   
+                    fg="black")
+label_Actor.pack(pady=10, padx=10, anchor="w")
+
+label_Director = Label(window, text="Director",
+                       anchor="w",
+                       font=("Arial", 16, "bold"),       
+                       bg="lightblue",   
+                       fg="black")
+label_Director.pack(pady=10, padx=10, anchor="w")
+
+window.mainloop()
