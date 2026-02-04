6e87cbda53214052b8b3a6aa988fa3a74b5a532d
Tymack2005 <fitz_ty@yahoo.com>
2026-02-04 10:22:54 +0000

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -3,5 +3,11 @@ window = Tk()
 
 window.title("Movie recommendation system")
 window.geometry("500x500")
+window.configure(background="black")
+tk.Label(text="genre").pack()
+tk.Label(text="actors").pack()
+tk.Label(text="director").pack()
+tk.Label(text="Rating").pack()
+tk.Label(text="Release Date").pack()
 
 window.mainloop()
