1696a5c0396ddab6b974d5c9b26c48f301a7fa8e
Isaac Diplock <dzaccy@outlook.com>
2026-02-11 10:37:37 +0000

    Not resizeable and yes no option works

--- GUI.py
+++ GUI.py
@@ -94,174 +94,175 @@ def Submit_Form():
     #popup
     message = "Is this correct? \n Genre: ", genre ,"\n Director: ", name, "\n Release Year: ", year ,"\n Director: ", langauge
     title = "Confirm"
-    ynbox(message, title)
-    
-    #recommendation window
-    recommendation_window = tk.Tk()
-    recommendation_window.title("Movie recommendation system - Recommendations")
-    recommendation_window.geometry("500x500")
-    recommendation_window.configure(background="lightblue")
-    recommendation_window.columnconfigure(0, weight=1)
-    recommendation_window.columnconfigure(1, weight=8)
-    recommendation_window.columnconfigure(2, weight=1)
-    recommendation_window.rowconfigure(0, weight=9)
-    recommendation_window.rowconfigure(1, weight=1)
-    
-    #left arrow button
-    left_arrow_img = tk.PhotoImage(master=recommendation_window, file="Images/Left Arrow.png")
-    left_arrow = tk.Button(recommendation_window,
-                           image=left_arrow_img,
-                            activebackground="lightblue",
-                            relief="flat",
-                            cursor="hand2",
-                            bg='lightblue')
-    left_arrow.grid(row=0, column=0)
-    
-    #middle section
-    recommendation_mid_section = tk.Frame(recommendation_window,
-                                     borderwidth=2,
-                                     bg='lightblue')
-    recommendation_mid_section.grid(row=0, column=1)
-    
-    #right arrow button
-    right_arrow_img = tk.PhotoImage(master=recommendation_window, file="Images/Right Arrow.png")
-    right_arrow = tk.Button(recommendation_window, 
-                            image=right_arrow_img,
-                            activebackground="lightblue",
-                            relief="flat",
-                            cursor="hand2",
-                            bg='lightblue')
-    right_arrow.grid(row=0, column=2)
-    
-    #white section
-    white_section = tk.Frame(recommendation_mid_section,
-                               padx=10, 
-                               pady=10,
-                               bg="white")
-    white_section.grid(row=0, column=0)
-    
-    #recommended movie attributes
-    recommended_movie_title = "Avatar"
-    recommended_movie_genre = "Action Adventure"
-    recommended_movie_director = "James Cameron"
-    recommended_movie_release_year = "2009"
-    recommended_movie_language = "English"
-    recommended_movie_rating = "7.2"
-    recommended_movie_runtime = "162 Minutes"
-    recommended_movie_description = "In the 22nd century, a paraplegic Marine is dispatched to the moon Pandora on a unique mission, but becomes torn between following orders and protecting an alien civilization."
-    recommended_movie_number = 1
-    
-    #movie title
-    recommended_movie_title_lbl = tk.Label(white_section,
-                                       text=recommended_movie_title,
-                                       font=("Arial", 24, "bold"),
-                                       bg='white')
-    recommended_movie_title_lbl.grid(row=0, column=0, columnspan=2, sticky=(tk.W))
-    
-    #movie genre
-    recommended_movie_genre_lbl = tk.Label(white_section,
-                                       text="Genre:",
-                                       font=("Arial", 10, "normal"),
-                                       bg='white')
-    recommended_movie_genre_lbl.grid(row=1, column=0, sticky=(tk.W))
-    recommended_movie_genre_entry = tk.Entry(white_section, font=('Arial', 10, 'normal'))
-    recommended_movie_genre_entry.insert(0, recommended_movie_genre)
-    recommended_movie_genre_entry.grid(row=1, column=1)
-    
-    #movie director
-    recommended_movie_director_lbl = tk.Label(white_section,
-                                       text="Director:",
-                                       font=("Arial", 10, "normal"),
-                                       bg='white')
-    recommended_movie_director_lbl.grid(row=2, column=0, sticky=(tk.W))
-    recommended_movie_director_entry = tk.Entry(white_section, font=('Arial', 10, 'normal'))
-    recommended_movie_director_entry.insert(0, recommended_movie_director)
-    recommended_movie_director_entry.grid(row=2, column=1)
-    
-    #movie release year
-    recommended_movie_release_year_lbl = tk.Label(white_section,
-                                       text="Release Year:",
-                                       font=("Arial", 10, "normal"),
-                                       bg='white')
-    recommended_movie_release_year_lbl.grid(row=3, column=0, sticky=(tk.W))
-    recommended_movie_release_year_entry = tk.Entry(white_section, font=('Arial', 10, 'normal'))
-    recommended_movie_release_year_entry.insert(0, recommended_movie_release_year)
-    recommended_movie_release_year_entry.grid(row=3, column=1)
-    
-    #movie language
-    recommended_movie_language_lbl = tk.Label(white_section,
-                                       text="Language:",
-                                       font=("Arial", 10, "normal"),
-                                       bg='white')
-    recommended_movie_language_lbl.grid(row=4, column=0, sticky=(tk.W))
-    recommended_movie_language_entry = tk.Entry(white_section, font=('Arial', 10, 'normal'))
-    recommended_movie_language_entry.insert(0, recommended_movie_language)
-    recommended_movie_language_entry.grid(row=4, column=1)
-    
-    #movie rating
-    recommended_movie_rating_lbl = tk.Label(white_section,
-                                       text="Rating:",
-                                       font=("Arial", 10, "normal"),
-                                       bg='white')
-    recommended_movie_rating_lbl.grid(row=5, column=0, sticky=(tk.W))
-    recommended_movie_rating_entry = tk.Entry(white_section, font=('Arial', 10, 'normal'))
-    recommended_movie_rating_entry.insert(0, recommended_movie_rating)
-    recommended_movie_rating_entry.grid(row=5, column=1)
-    
-    #movie runtime
-    recommended_movie_runtime_lbl = tk.Label(white_section,
-                                       text="Runtime:",
-                                       font=("Arial", 10, "normal"),
-                                       bg='white')
-    recommended_movie_runtime_lbl.grid(row=6, column=0, sticky=(tk.W))
-    recommended_movie_runtime_entry = tk.Entry(white_section, font=('Arial', 10, 'normal'))
-    recommended_movie_runtime_entry.insert(0, recommended_movie_runtime)
-    recommended_movie_runtime_entry.grid(row=6, column=1)
-    
-    #movie description
-    recommended_movie_description_lbl = tk.Label(white_section,
-                                       text="Description:",
-                                       font=("Arial", 10, "normal"),
-                                       bg='white')
-    recommended_movie_description_lbl.grid(row=7, column=0, sticky=(tk.W))
-    recommended_movie_description_tb = tk.Text(white_section, 
-                                               width=40,
-                                               height=5,
-                                               font=('Arial', 10, 'normal'))
-    recommended_movie_description_tb.insert("1.0", recommended_movie_description)
-    recommended_movie_description_tb.grid(row=8, column=0, columnspan=2, sticky=(tk.W))
-    
-    #select button
-    select_button = tk.Button(white_section,
-                            text="Select",
-                            activebackground="darkgrey",
-                            activeforeground="white",
-                            anchor="center",
-                            bd=3,
-                            bg="darkblue",
-                            cursor="hand2",
-                            fg="white",
-                            font=("Arial", 12, "bold"),
-                            height=1,
-                            highlightbackground="black",
-                            highlightcolor="green",
-                            highlightthickness=2,
-                            justify="left",
-                            overrelief="raised",
-                            padx=10,
-                            pady=5,
-                            width=15,
-                            wraplength=100)
-    select_button.grid(row=9, column=0, columnspan=3)
-    
-    #recommended movie counter label.
-    recommendation_counter = tk.Label(recommendation_mid_section, 
-                    text= str(recommended_movie_number) + " out of 10",
-                    font=("Arial", 8, "bold"),
-                    bg='lightblue')
-    recommendation_counter.grid(row=1, column=0, sticky=(tk.S))
-    
-    recommendation_window.mainloop()
+    output = ynbox(message, title)
+    if output: 
+        #recommendation window
+        recommendation_window = tk.Tk()
+        recommendation_window.title("Movie recommendation system - Recommendations")
+        recommendation_window.geometry("500x500")
+        recommendation_window.configure(background="lightblue")
+        recommendation_window.columnconfigure(0, weight=1)
+        recommendation_window.columnconfigure(1, weight=8)
+        recommendation_window.columnconfigure(2, weight=1)
+        recommendation_window.rowconfigure(0, weight=9)
+        recommendation_window.rowconfigure(1, weight=1)
+        recommendation_window.resizable(0,0)
+        
+        #left arrow button
+        left_arrow_img = tk.PhotoImage(master=recommendation_window, file="C:/Users/25719211/Downloads/Left Arrow.png")
+        left_arrow = tk.Button(recommendation_window,
+                               image=left_arrow_img,
+                                activebackground="lightblue",
+                                relief="flat",
+                                cursor="hand2",
+                                bg='lightblue')
+        left_arrow.grid(row=0, column=0)
+        
+        #middle section
+        recommendation_mid_section = tk.Frame(recommendation_window,
+                                         borderwidth=2,
+                                         bg='lightblue')
+        recommendation_mid_section.grid(row=0, column=1)
+        
+        #right arrow button
+        right_arrow_img = tk.PhotoImage(master=recommendation_window, file="C:/Users/25719211/Downloads/Right Arrow.png")
+        right_arrow = tk.Button(recommendation_window, 
+                                image=right_arrow_img,
+                                activebackground="lightblue",
+                                relief="flat",
+                                cursor="hand2",
+                                bg='lightblue')
+        right_arrow.grid(row=0, column=2)
+        
+        #white section
+        white_section = tk.Frame(recommendation_mid_section,
+                                   padx=10, 
+                                   pady=10,
+                                   bg="white")
+        white_section.grid(row=0, column=0)
+        
+        #recommended movie attributes
+        recommended_movie_title = "Avatar"
+        recommended_movie_genre = "Action Adventure"
+        recommended_movie_director = "James Cameron"
+        recommended_movie_release_year = "2009"
+        recommended_movie_language = "English"
+        recommended_movie_rating = "7.2"
+        recommended_movie_runtime = "162 Minutes"
+        recommended_movie_description = "In the 22nd century, a paraplegic Marine is dispatched to the moon Pandora on a unique mission, but becomes torn between following orders and protecting an alien civilization."
+        recommended_movie_number = 1
+        
+        #movie title
+        recommended_movie_title_lbl = tk.Label(white_section,
+                                           text=recommended_movie_title,
+                                           font=("Arial", 24, "bold"),
+                                           bg='white')
+        recommended_movie_title_lbl.grid(row=0, column=0, columnspan=2, sticky=(tk.W))
+        
+        #movie genre
+        recommended_movie_genre_lbl = tk.Label(white_section,
+                                           text="Genre:",
+                                           font=("Arial", 10, "normal"),
+                                           bg='white')
+        recommended_movie_genre_lbl.grid(row=1, column=0, sticky=(tk.W))
+        recommended_movie_genre_entry = tk.Entry(white_section, font=('Arial', 10, 'normal'))
+        recommended_movie_genre_entry.insert(0, recommended_movie_genre)
+        recommended_movie_genre_entry.grid(row=1, column=1)
+        
+        #movie director
+        recommended_movie_director_lbl = tk.Label(white_section,
+                                           text="Director:",
+                                           font=("Arial", 10, "normal"),
+                                           bg='white')
+        recommended_movie_director_lbl.grid(row=2, column=0, sticky=(tk.W))
+        recommended_movie_director_entry = tk.Entry(white_section, font=('Arial', 10, 'normal'))
+        recommended_movie_director_entry.insert(0, recommended_movie_director)
+        recommended_movie_director_entry.grid(row=2, column=1)
+        
+        #movie release year
+        recommended_movie_release_year_lbl = tk.Label(white_section,
+                                           text="Release Year:",
+                                           font=("Arial", 10, "normal"),
+                                           bg='white')
+        recommended_movie_release_year_lbl.grid(row=3, column=0, sticky=(tk.W))
+        recommended_movie_release_year_entry = tk.Entry(white_section, font=('Arial', 10, 'normal'))
+        recommended_movie_release_year_entry.insert(0, recommended_movie_release_year)
+        recommended_movie_release_year_entry.grid(row=3, column=1)
+        
+        #movie language
+        recommended_movie_language_lbl = tk.Label(white_section,
+                                           text="Language:",
+                                           font=("Arial", 10, "normal"),
+                                           bg='white')
+        recommended_movie_language_lbl.grid(row=4, column=0, sticky=(tk.W))
+        recommended_movie_language_entry = tk.Entry(white_section, font=('Arial', 10, 'normal'))
+        recommended_movie_language_entry.insert(0, recommended_movie_language)
+        recommended_movie_language_entry.grid(row=4, column=1)
+        
+        #movie rating
+        recommended_movie_rating_lbl = tk.Label(white_section,
+                                           text="Rating:",
+                                           font=("Arial", 10, "normal"),
+                                           bg='white')
+        recommended_movie_rating_lbl.grid(row=5, column=0, sticky=(tk.W))
+        recommended_movie_rating_entry = tk.Entry(white_section, font=('Arial', 10, 'normal'))
+        recommended_movie_rating_entry.insert(0, recommended_movie_rating)
+        recommended_movie_rating_entry.grid(row=5, column=1)
+        
+        #movie runtime
+        recommended_movie_runtime_lbl = tk.Label(white_section,
+                                           text="Runtime:",
+                                           font=("Arial", 10, "normal"),
+                                           bg='white')
+        recommended_movie_runtime_lbl.grid(row=6, column=0, sticky=(tk.W))
+        recommended_movie_runtime_entry = tk.Entry(white_section, font=('Arial', 10, 'normal'))
+        recommended_movie_runtime_entry.insert(0, recommended_movie_runtime)
+        recommended_movie_runtime_entry.grid(row=6, column=1)
+        
+        #movie description
+        recommended_movie_description_lbl = tk.Label(white_section,
+                                           text="Description:",
+                                           font=("Arial", 10, "normal"),
+                                           bg='white')
+        recommended_movie_description_lbl.grid(row=7, column=0, sticky=(tk.W))
+        recommended_movie_description_tb = tk.Text(white_section, 
+                                                   width=40,
+                                                   height=5,
+                                                   font=('Arial', 10, 'normal'))
+        recommended_movie_description_tb.insert("1.0", recommended_movie_description)
+        recommended_movie_description_tb.grid(row=8, column=0, columnspan=2, sticky=(tk.W))
+        
+        #select button
+        select_button = tk.Button(white_section,
+                                text="Select",
+                                activebackground="darkgrey",
+                                activeforeground="white",
+                                anchor="center",
+                                bd=3,
+                                bg="darkblue",
+                                cursor="hand2",
+                                fg="white",
+                                font=("Arial", 12, "bold"),
+                                height=1,
+                                highlightbackground="black",
+                                highlightcolor="green",
+                                highlightthickness=2,
+                                justify="left",
+                                overrelief="raised",
+                                padx=10,
+                                pady=5,
+                                width=15,
+                                wraplength=100)
+        select_button.grid(row=9, column=0, columnspan=3)
+        
+        #recommended movie counter label.
+        recommendation_counter = tk.Label(recommendation_mid_section, 
+                        text= str(recommended_movie_number) + " out of 10",
+                        font=("Arial", 8, "bold"),
+                        bg='lightblue')
+        recommendation_counter.grid(row=1, column=0, sticky=(tk.S))
+        
+        recommendation_window.mainloop()
 
 #search button label
 button = tk.Button(window,
@@ -286,8 +287,6 @@ button = tk.Button(window,
                    width=15,
                    wraplength=100)
 button.pack(padx=20, pady=20)
-
-
 
 window.resizable(0,0)
 
